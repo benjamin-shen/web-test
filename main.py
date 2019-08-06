f70e9d9846bd2f47d73b8732629f46f2c1e3ca0d
Benjamin Shen <bfs45@cornell.edu>
2019-08-06 16:02:32 -0400

    flask

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ def index():
     file = open("index.html", "r")
     return file.read()
 
+@app.route('/login.py')
+def index():
+    import login
+    return login.result()
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_dir(path):
+    return path
+
 if __name__ == '__main__':
    app.run(debug = True)
